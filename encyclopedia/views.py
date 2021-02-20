ef19c4ad530aed24e4e411c902e530e5dd3fdfab
Parker Sullins <parker.rsullins@gmail.com>
2021-02-19 22:35:43 -0600

    Changed Project Name

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,5 +9,6 @@ def index(request):
 
 def create_page(request):
     return render(request, "encyclopedia/create_page.html")
-#../../../encyclopedia/static/encyclopedia/styles.css
-#<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+def entry(request):
+    return render(request, "encyclopedia/entry.html")
