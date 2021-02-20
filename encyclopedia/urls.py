92eceb775ccf1c883e6f75a1adcac8dd06b4f283
Parker Sullins <51936825+Parker-Sullins@users.noreply.github.com>
2021-02-19 23:21:14 -0600

    Added Dynamic URLS, & ability to edit post

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 app_name = "encyclopedia"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("create_page/", views.create_page, name="create_page"),
-    path("entry/", views.entry, name="entry")
+    path("", views.index, name="home"),
+    path("entry/<str:title>/", views.entry, name="entry"),
+    path("contribute/", views.contribute, name="contribute"),
+    path("entry/<str:title>/edit/", views.edit, name="edit")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,18 +1,68 @@
 from django.shortcuts import render
 from . import util
 from markdown2 import markdown, markdown_path
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Enter Title")
+    description = forms.CharField(label="Enter Description")
+
+
+class EditForm(forms.Form):
+    description = forms.CharField(label="Edit Description")
+
 
 def index(request):
+    request.session["entries"] = util.list_entries()
     return render(request, "encyclopedia/home.html", {
-        "entries": util.list_entries()
+        "entries": request.session["entries"]
     })
 
-def create_page(request):
-    return render(request, "encyclopedia/create_page.html")
+
+def entry(request, title):
+    title = title
+    entry = markdown_path('media/entries/' + title + '.md', extras=["break-on-newline"])
+    return render(request, "encyclopedia/entry.html", {
+        'entry': entry,
+        'title': title
+    })
 
 
-def entry(request):
-    name = util.list_entries()
-    name = str(name[4])
-    hey = markdown_path('media/entries/' + name + '.md')
-    return render(request, "encyclopedia/entry.html", {'entry': hey})
+def contribute(request):
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            description = form.cleaned_data["description"]
+            title = form.cleaned_data["title"]
+            description_formatted = '#' + title.title() + '\n' + description.capitalize()
+            request.session['new_entry'] = util.save_entry(title, description_formatted)
+            request.session["entries"].append(request.session['new_entry'])
+            return HttpResponseRedirect(reverse("encyclopedia:home"))
+        else:
+            return render(request, "encyclopedia/contribute.html", {
+                "form": form
+            })
+    return render(request, "encyclopedia/contribute.html", {
+            'form': NewEntryForm()
+            })
+
+
+def edit(request, title):
+    form = EditForm()
+    if request.method == "POST":
+        form = EditForm(request.POST)
+        if form.is_valid():
+            description = form.cleaned_data["description"]
+            title_2 = title
+            description_formatted = '#' + title_2.title() + '\n' + description.capitalize()
+            util.save_entry(title_2, description_formatted)
+            return HttpResponseRedirect(reverse("encyclopedia:home"))
+        else:
+            return render(request, "encyclopedia/contribute.html", {"form": form})
+    return render(request, "encyclopedia/edit.html", {
+        'title': title,
+        'form': form
+    })
